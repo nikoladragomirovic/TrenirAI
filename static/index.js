d62f22d5b5975945ac2d2bc8cebaadf2f0ea4361
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-10-08 15:36:26 +0200

    Formatting and Send Button Fix

--- static/index.js
+++ static/index.js
@@ -1,104 +1,118 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const preloader = document.getElementById("preloader");
+  const img = document.getElementById("logo");
+  const navigation = document.getElementById("navigation");
 
-    const preloader = document.getElementById('preloader');
-    const img = document.getElementById('logo')
-    const navigation = document.getElementById('navigation');
+  const anchor = document.getElementById("graphic");
+  const rect = anchor.getBoundingClientRect();
+  const anchorX = rect.left + rect.width / 2;
+  const anchorY = rect.top + rect.height / 2;
 
-    const anchor = document.getElementById('graphic');
-    const rect = anchor.getBoundingClientRect();
-    const anchorX = rect.left + rect.width / 2;
-    const anchorY = rect.top + rect.height / 2;
+  const button = document.getElementById("image");
+  const email = document.getElementById("email");
 
-    const button = document.getElementById('image');
-    const email = document.getElementById('email');
+  button.addEventListener("click", function () {
+    button.classList.add("animate");
+    email.classList.add("disable");
 
-    button.addEventListener('click', function() {
+    setTimeout(() => {
+      button.classList.remove("animate");
+      email.classList.remove("disable");
+    }, 1000);
+  });
 
-        button.classList.add('animate');
-        email.classList.add('disable');
+  document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
 
-        setTimeout(() => {
+    const moveX = -(anchorX - mouseX);
+    const moveY = -(anchorY - mouseY);
 
-            button.classList.remove('animate');
-            email.classList.remove('disable');
+    anchor.style.transform = `translate(${moveX / 25}px, ${
+      moveY / 25
+    }px) scaleX(-1) scale(1.7)`;
+  });
 
-        }, 1000);
+  applyCardHoverEffects(
+    "#left-card",
+    "#middle-card",
+    "60",
+    "#right-card",
+    "60"
+  );
+  applyCardHoverEffects(
+    "#middle-card",
+    "#left-card",
+    "-60",
+    "#right-card",
+    "60"
+  );
+  applyCardHoverEffects(
+    "#right-card",
+    "#middle-card",
+    "-60",
+    "#left-card",
+    "-60"
+  );
 
-    });
-
-    document.addEventListener('mousemove', (e) => {
-        
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-    
-        const moveX = -(anchorX - mouseX);
-        const moveY = -(anchorY - mouseY);
-    
-        anchor.style.transform = `translate(${moveX/25}px, ${moveY/25}px) scaleX(-1) scale(1.7)`;
-    
-    });
-
-    applyCardHoverEffects('#left-card', '#middle-card', '60', '#right-card', '60');
-    applyCardHoverEffects('#middle-card', '#left-card', '-60', '#right-card', '60');
-    applyCardHoverEffects('#right-card', '#middle-card', '-60', '#left-card', '-60');
-
-    hidePreloader(preloader, img, navigation);
-    smoothScroll();
-
+  hidePreloader(preloader, img, navigation);
+  smoothScroll();
 });
 
-function hidePreloader(preloader, img, navigation){
-    if (preloader) {
-        setTimeout(function () {
-            preloader.classList.add('hidden');
-            img.style.opacity = '1';
-            navigation.style.opacity = '1';
+function hidePreloader(preloader, img, navigation) {
+  if (preloader) {
+    setTimeout(function () {
+      preloader.classList.add("hidden");
+      img.style.opacity = "1";
+      navigation.style.opacity = "1";
 
-            setTimeout(function (){
-                preloader.style.display = 'none';
-                document.body.style.overflow = 'auto';
-            }, 500);
-    
-        }, 0);
-    }
+      setTimeout(function () {
+        preloader.style.display = "none";
+        document.body.style.overflow = "auto";
+      }, 500);
+    }, 0);
+  }
 }
 
-function smoothScroll(){
-    document.querySelectorAll('nav a').forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
+function smoothScroll() {
+  document.querySelectorAll("nav a").forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
 
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
 
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - document.querySelector('nav').offsetHeight/2, 
-                    behavior: 'smooth'
-                });
-            }
+      if (targetElement) {
+        window.scrollTo({
+          top:
+            targetElement.offsetTop -
+            document.querySelector("nav").offsetHeight / 2,
+          behavior: "smooth",
         });
+      }
     });
+  });
 }
 
 function applyCardHoverEffects(mainCard, sideCard1, move1, sideCard2, move2) {
-    $(mainCard).hover(function () {
-    $(this).css('transform', 'scale(1.4)');
-    $(this).find('.price').css('transform', 'scale(1.3)');
-    $(this).find('.price').css('color', 'rgb(255, 126, 0)');
+  $(mainCard).hover(
+    function () {
+      $(this).css("transform", "scale(1.4)");
+      $(this).find(".price").css("transform", "scale(1.3)");
+      $(this).find(".price").css("color", "rgb(255, 126, 0)");
 
-    $(sideCard1).css('transform', `translateX(${move1}px)`);
-    $(sideCard2).css('transform', `translateX(${move2}px)`);
-  
-    }, function () {
-    $(this).css('transform', 'none');
-    $(this).find('.price').css('transform', 'none');
-    $(this).find('.price').css('color', '#8a8a8a');
+      $(sideCard1).css("transform", `translateX(${move1}px)`);
+      $(sideCard2).css("transform", `translateX(${move2}px)`);
+    },
+    function () {
+      $(this).css("transform", "none");
+      $(this).find(".price").css("transform", "none");
+      $(this).find(".price").css("color", "#8a8a8a");
 
-    $(sideCard1).css('transform', 'none');
-    $(sideCard2).css('transform', 'none');
+      $(sideCard1).css("transform", "none");
+      $(sideCard2).css("transform", "none");
 
-    $('#middle-card').css('transform', 'scale(1.4)');
-
-    });
-  }
+      $("#middle-card").css("transform", "scale(1.4)");
+    }
+  );
+}
